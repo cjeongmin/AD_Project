33e6ffb194039061a4b1da7fe4693625e1536c78
CJM <cwa7510@kookmin.ac.kr>
2019-11-14 11:38:30 +0900

    files

---
+++ chess/position.py
@@ -0,0 +1,10 @@
+class Position:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"[{self.x}, {self.y}]"
+
+if __name__ == "__main__":
+    print(Position(5, 3))
