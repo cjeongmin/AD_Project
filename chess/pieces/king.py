d909c65aaa5da86e552979f830a00daddc431efa
CJM <cwa7510@kookmin.ac.kr>
2019-11-18 16:45:13 +0900

    king.py reupdate

--- chess/pieces/king.py
+++ chess/pieces/king.py
@@ -3,9 +3,10 @@ sys.path.append('.')
 
 from .piece import Piece
 from ..position import Position
+from ..team import Team
 
 class King(Piece):
-    def __init__(self, pos: Position, team=str):
+    def __init__(self, pos: Position, team=Team):
         super().__init__(pos, team)
         self.isChecked = False
         
