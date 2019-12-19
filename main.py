bd5a4cc836e70ed3bc59ed85692664e9d9977a3a
Winjae <hsj9830@kookmin.ac.kr>
2019-12-19 14:54:49 +0900

    .

--- main.py
+++ main.py
@@ -3,16 +3,9 @@ import sys
 from time import sleep
 from os import system
 
-from chess.pieces.pawn import Pawn
-from chess.pieces.bishop import Bishop
-from chess.pieces.knight import Knight
-from chess.pieces.rook import Rook
-from chess.pieces.queen import Queen
-from chess.pieces.king import King
-
-from chess.position import Position
-from chess.team import Team
 from chess.check import fillCheckBoard
+
+from unittestChess.chessBoard import *
 
 from gui.board import Board
 from PyQt5.QtWidgets import QApplication
@@ -72,42 +65,5 @@ def guiTest(chessBoard):
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
-    # chessBoard = [
-    #     [
-    #         Rook(Position(0, 0), Team.BLACK), Knight(Position(1, 0), Team.BLACK), Bishop(Position(2, 0), Team.BLACK), Queen(Position(3, 0), Team.BLACK), King(Position(4, 0), Team.BLACK), Bishop(Position(5, 0), Team.BLACK), Knight(Position(6, 0), Team.BLACK), Rook(Position(7, 0), Team.BLACK)
-    #     ],
-    #     [
-    #         Pawn(Position(x, 1), Team.BLACK) for x in range(8)
-    #     ],
-    #     [None for _ in range(8)],
-    #     [None for _ in range(8)],
-    #     [None for _ in range(8)],
-    #     [None for _ in range(8)],
-    #     [
-    #         Pawn(Position(x, 6), Team.WHITE) for x in range(8)
-    #     ],
-    #     [
-    #         Rook(Position(0, 7), Team.WHITE), Knight(Position(1, 7), Team.WHITE), Bishop(Position(2, 7), Team.WHITE), Queen(Position(3, 7), Team.WHITE), King(Position(4, 7), Team.WHITE), Bishop(Position(5, 7), Team.WHITE), Knight(Position(6, 7), Team.WHITE), Rook(Position(7, 7), Team.WHITE)
-    #     ],
-        
-    # ]
-    chessBoard = [
-        [
-            None, Bishop(Position(1, 0), Team.BLACK), None, None, King(Position(4, 0), Team.BLACK), None, None, None
-        ],
-        [
-            None, None, None, None, None, None, None, None, None
-        ],
-        [None for _ in range(8)],
-        [None for _ in range(8)],
-        [None for _ in range(8)],
-        [None for _ in range(8)],
-        [
-            None, None, None, None, None, None, None, None, None
-        ],
-        [
-            None, None, None, None,  King(Position(4, 7), Team.WHITE), None, None,  Rook(Position(7, 7), Team.WHITE)
-        ],
-    ]
-    guiTest(chessBoard)
+    guiTest(chessBoard_promotion)
     #textTest(chessBoard)
